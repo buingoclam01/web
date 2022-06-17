c4c5ca2c39dce2c0a91bc3123676cbe08a5de814
buingoclam <lb130801@gmail.com>
2022-06-17 10:19:54 +0700

    up leen

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,7 +7,7 @@
         <title>Laravel</title>
 
         <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+        <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" rel="stylesheet">
 
         <!-- Styles -->
         <style>
